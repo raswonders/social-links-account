f389cc176cd931631f6e61382249fc63a1898424
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-10 20:30:20 +0200

    add hover effect

--- src/SocialLink.tsx
+++ src/SocialLink.tsx
@@ -8,8 +8,8 @@ type SocialLinkProps = {
 export function SocialLink({ href, children }: SocialLinkProps) {
   return (
     <a href={href}>
-      <div className="w-full p-3 bg-grey rounded-lg justify-center items-center gap-2 inline-flex">
-        <div className="text-white text-sm font-bold font-custom leading-normal">
+      <div className="w-full p-3 bg-grey hover:bg-neonGreen text-white hover:text-grey rounded-lg justify-center items-center gap-2 inline-flex">
+        <div className="text-sm font-bold font-custom leading-normal">
           {children}
         </div>
       </div>
