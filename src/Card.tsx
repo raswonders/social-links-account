e8e117a03e48ca49baa7bb4edd8489494bafaf2c
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-10 20:39:04 +0200

    make card responsive

--- src/Card.tsx
+++ src/Card.tsx
@@ -3,23 +3,23 @@ import avatarImage from "/public/assets/images/avatar-jessica.jpeg";
 
 export function Card() {
   return (
-    <div className="w-80 p-6 bg-darkGrey rounded-xl flex-col justify-start items-center gap-6 inline-flex">
+    <div className="w-80 sm:w-96 sm:p-10 p-6 sm:shrink-0 bg-darkGrey rounded-xl flex-col justify-start gap-6 inline-flex text-center">
       <img
-        className="w-[88px] h-[88px] relative rounded-[999px]"
+        className="w-[88px] self-center relative rounded-full"
         src={avatarImage}
       />
-      <div className="self-stretch h-[61px] flex-col justify-start items-center gap-1 flex">
-        <div className="self-stretch text-center text-white text-2xl font-semibold font-custom leading-9">
+      <div className="flex-col justify-start items-center gap-1 flex">
+        <div className="text-white text-2xl font-semibold font-custom leading-9">
           Jessica Randall
         </div>
-        <div className="self-stretch text-center text-neonGreen text-sm font-bold font-custom leading-[21px]">
+        <div className="text-neonGreen text-sm font-bold font-custom leading-normal">
           London, United Kingdom
         </div>
       </div>
-      <div className="self-stretch text-center text-white text-sm font-normal font-custom leading-[21px]">
+      <div className="text-white text-sm font-normal font-custom leading-normal">
         "Front-end developer and avid reader."
       </div>
-      <div className="self-stretch h-[289px] flex-col justify-start items-stretch gap-4 flex">
+      <div className="flex-col justify-start items-stretch gap-4 flex">
         <SocialLink href="https://github.com/raswonders">GitHub</SocialLink>
         <SocialLink href="https://www.frontendmentor.io/profile/raswonders">
           Frontend Mentor
